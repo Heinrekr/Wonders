75a382c1410dd56206a5c56f41d4f2ebb21503b9
Genrih <genrihbernd@gmail.com>
2025-09-09 15:30:33 +0300

    counter

---
+++ script.js
@@ -0,0 +1,15 @@
+//СЧЕТЧИК ПАССАЖИРОВ
+const counterText = document.querySelector('.passenger-number');
+const counterButton = document.querySelectorAll('.passenger-button');
+let counter = 1;
+
+counterButton.forEach( (el,index) => {
+    el.addEventListener('click', (e) => {
+        if (index === 0 && counter > 1) {
+            counter = counter - 1;
+        } else if (index === 1 && counter < 12) {
+            counter = counter + 1;
+        }
+        counterText.textContent = counter;
+    });
+});
